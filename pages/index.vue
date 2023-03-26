3147aba6df4800a5ea60de059cb7364590c9f1f5
yimeisuren <2456387473@qq.com>
2023-03-26 14:28:31 +0800

    项目基本配置成功, 可以正常启动

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
-
     <div>
         <Swiper/>
-
         <div id="aCoursesList">
             <HotClass/>
             <HotTeacher/>
@@ -11,21 +9,5 @@
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            swiperOption: {
-                //配置分页
-                pagination: {
-                    el: '.swiper-pagination'//分页的dom节点
-                },
-                //配置导航
-                navigation: {
-                    nextEl: '.swiper-button-next',//下一页dom节点
-                    prevEl: '.swiper-button-prev'//前一页dom节点
-                }
-            }
-        }
-    }
-}
+export default {}
 </script>
